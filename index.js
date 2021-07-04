ba6671d0ebeec6e1c60b95cb5228a3cde162f75f
Guillaume <Gui.lefort.17@gmail.com>
2021-07-04 17:39:52 +0200

    Revert " delete previous module"
    This reverts commit 9cc3b67fe37913caf8c57dc40473508e1744bc06.

--- index.js
+++ index.js
@@ -2,32 +2,31 @@ const express = require('express');
 const app = express();
 const data = require('./dataCowork.json');
 const cities = require('./dataCity');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
-  response.send('Hello, welcome Hackton X fievrr API');
+app.get('/', (req, res) => {
+  res.send('Hello, welcome Hackton X fievrr API');
 });
 
-app.get('/datacowork', (request, response) => {
-  response.status(200).json(data.results);
+app.get('/datacowork', (req, res) => {
+  res.status(200).json(data.results);
 });
 
-app.get('/cities', (request, response) => {
-  response.status(200).json(cities.results);
+app.get('/cities', (req, res) => {
+  res.status(200).json(cities.results);
 });
 
-app.get('/datacowork/:id', (request, response) => {
-  const dataCowork = data.results.find((d) => d.id == request.params.id);
+app.get('/datacowork/:id', (req, res) => {
+  const dataCowork = data.results.find((d) => d.id == req.params.id);
   if (dataCowork) {
-    response.status(200).send(dataCowork);
+    res.status(200).send(dataCowork);
   } else {
     res.status(404).send(`not found...`);
   }
 });
-
-app.listen(port, (error) => {
-  if (error) {
-    console.error(error);
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
   } else {
     console.log(`Express server listening on ${port}`);
   }
