9cc3b67fe37913caf8c57dc40473508e1744bc06
Guillaume <Gui.lefort.17@gmail.com>
2021-07-04 17:35:26 +0200

    delete previous module

--- index.js
+++ index.js
@@ -2,31 +2,32 @@ const express = require('express');
 const app = express();
 const data = require('./dataCowork.json');
 const cities = require('./dataCity');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello, welcome Hackton X fievrr API');
+app.get('/', (request, response) => {
+  response.send('Hello, welcome Hackton X fievrr API');
 });
 
-app.get('/datacowork', (req, res) => {
-  res.status(200).json(data.results);
+app.get('/datacowork', (request, response) => {
+  response.status(200).json(data.results);
 });
 
-app.get('/cities', (req, res) => {
-  res.status(200).json(cities.results);
+app.get('/cities', (request, response) => {
+  response.status(200).json(cities.results);
 });
 
-app.get('/datacowork/:id', (req, res) => {
-  const dataCowork = data.results.find((d) => d.id == req.params.id);
+app.get('/datacowork/:id', (request, response) => {
+  const dataCowork = data.results.find((d) => d.id == request.params.id);
   if (dataCowork) {
-    res.status(200).send(dataCowork);
+    response.status(200).send(dataCowork);
   } else {
     res.status(404).send(`not found...`);
   }
 });
-app.listen(port, (err) => {
-  if (err) {
-    console.error(err);
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error(error);
   } else {
     console.log(`Express server listening on ${port}`);
   }
